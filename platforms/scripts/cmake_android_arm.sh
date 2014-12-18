d7fd3906b75e01f5d6702902a68acc0666ac6544
Alexander Smorkalov <alexander.smorkalov@itseez.com>
2014-12-18 20:35:14 +0300

    Some hacks for ARMv8a support.

--- platforms/scripts/cmake_android_arm.sh
+++ platforms/scripts/cmake_android_arm.sh
@@ -4,4 +4,4 @@ cd `dirname $0`/..
 mkdir -p build_android_arm
 cd build_android_arm
 
-cmake -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_TOOLCHAIN_FILE=../android/android.toolchain.cmake $@ ../..
+cmake -DANDROID_ABI="arm64-v8a" -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_TOOLCHAIN_FILE=../android/android.toolchain.cmake $@ ../..
